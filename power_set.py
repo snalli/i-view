d1a8bce266ee2d9bfcae0e2a46e5aab9a464d35b
Sanketh Nalli <nalli@wisc.edu>
2019-01-22 21:48:27 +0530

    commit - 2

---
+++ power_set.py
@@ -0,0 +1,21 @@
+def all_subseq(prefix, suffix, seen):
+
+	s = ''.join(prefix)
+	if s in seen:
+		return
+
+	seen.add(s)
+	for i,s in enumerate(suffix):
+		all_subseq(prefix + [s], suffix[i+1:], seen)
+
+answer = set()
+s = "abc"
+all_subseq([], list(s), answer) 
+for a in sorted(list(answer)): 
+	print a
+
+answer = set()
+s = "aab"
+all_subseq([], list(s), answer) 
+for a in sorted(list(answer)): 
+	print a
